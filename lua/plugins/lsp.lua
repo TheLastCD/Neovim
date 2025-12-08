023f16f3186e9eef9f08262d39b6989a511ccb8f
charlie.elliott <charlie.elliott@connectorsubsea.com>
2025-12-08 15:38:59 +0000

    Patch: Jedi issues with my latest container configs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,63 +1,66 @@
 require("mason-lspconfig").setup({
-  ensure_installed = { "pyright", "lua_ls" },
-  automatic_installation = true,
-  handlers = {
-    function(server_name) -- default handler
-      vim.lsp.config[server_name].setup({})
-    end,
-  },
+	ensure_installed = { "pyright", "lua_ls" },
+	automatic_installation = true,
+	handlers = {
+		function(server_name) -- default handler
+			vim.lsp.config[server_name].setup({})
+		end,
+	},
 })
 
 vim.lsp.config.lua_ls = {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },  -- fix "undefined global vim" warnings
-      },
-      runtime = { version = "LuaJIT" },
-      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-      telemetry = { enable = false },
-    },
-  },
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" }, -- fix "undefined global vim" warnings
+			},
+			runtime = { version = "LuaJIT" },
+			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+			telemetry = { enable = false },
+		},
+		pylsp = {
+			plugins = {
+				jedi_completion = { enabled = false },
+				jedi_definition = { enabled = false },
+				jedi_hover = { enabled = false },
+			},
+		},
+	},
 }
 vim.lsp.enable("lua_ls")
 
-
 vim.diagnostic.config({
-  signs = {
-    severity = {
-      [vim.diagnostic.severity.ERROR] = "",
-      [vim.diagnostic.severity.WARN] = "",
-      [vim.diagnostic.severity.INFO] = "",
-      [vim.diagnostic.severity.HINT] = "",
-    },
-    -- Optionally numhl and linehl can be configured similarly
-  },
+	signs = {
+		severity = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "",
+		},
+		-- Optionally numhl and linehl can be configured similarly
+	},
 })
 vim.lsp.config.ts_ls = {
-  init_options = {
-    preferences = {
-      disableSuggestions = false,
-    },
-    noErrorTruncation = true,
-  },
+	init_options = {
+		preferences = {
+			disableSuggestions = false,
+		},
+		noErrorTruncation = true,
+	},
 }
 vim.lsp.enable("ts_ls")
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = '● ',  -- change diagnostic inline prefix icon
-    spacing = 4,   -- spacing between text and prefix
-  }, -- enable virtual text (inline error messages)
-  signs = true,          -- show signs in the gutter
-  underline = true,      -- underline errors/warnings
-  update_in_insert = false,
-  severity_sort = true,
-  float = {
-    source = "always",   -- show source in floating diagnostics
-    border = "rounded",
-  },
+	virtual_text = {
+		prefix = "● ", -- change diagnostic inline prefix icon
+		spacing = 4, -- spacing between text and prefix
+	}, -- enable virtual text (inline error messages)
+	signs = true, -- show signs in the gutter
+	underline = true, -- underline errors/warnings
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		source = "always", -- show source in floating diagnostics
+		border = "rounded",
+	},
 })
-
-
-
